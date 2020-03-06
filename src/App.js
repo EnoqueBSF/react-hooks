e26058f9d10ac4a4264a1e1191311ecbd1506732
Enoque Belmiro dos Santos Filho <enoquebelmiro@hotmail.com>
2020-03-06 02:56:29 -0300

    Finish

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
+
+function App() {
+  // Simulando o state => setState
+  const [techs, setTech] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTech([...techs, newTech]);
+    setNewTech('');
+  }, [newTech, techs]);
+
+  // Simulando DidMount
+  useEffect(() => {
+    const storageTech = localStorage.getItem('techs');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  // Simulando DidUpdate
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
+
+  const techSize = useMemo(() => techs.length, [techs]);
+
+  return (
+    <>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <strong>Você tem {techSize}</strong>
+      <br />
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
+}
+
+export default App;
